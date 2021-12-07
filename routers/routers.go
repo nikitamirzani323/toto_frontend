852eb4c86fdbf61aa37dc20608b67c8ecf3ee566
ncooky <mp_ksa@lidowin.com>
2021-12-07 14:45:44 +0700

    add owasp pentest header protection

--- routers/routers.go
+++ routers/routers.go
@@ -10,6 +10,18 @@ import (
 
 func Init() *fiber.App {
 	app := fiber.New()
+	app.Use(func(c *fiber.Ctx) error {
+		// Set some security headers:
+		c.Set("X-XSS-Protection", "1; mode=block")
+		c.Set("X-Content-Type-Options", "nosniff")
+		c.Set("X-Download-Options", "noopen")
+		c.Set("Strict-Transport-Security", "max-age=5184000")
+		c.Set("X-Frame-Options", "SAMEORIGIN")
+		c.Set("X-DNS-Prefetch-Control", "off")
+
+		// Go to next middleware:
+		return c.Next()
+	})
 	app.Use(logger.New())
 	app.Use(recover.New())
 	app.Use(compress.New())
