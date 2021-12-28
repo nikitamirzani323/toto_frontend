4ab8e4ea42c6c923a13f9bdfe3e028c33549e060
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2021-12-28 22:49:59 +0700

    Update Domain Security

--- controller/main-controller.go
+++ controller/main-controller.go
@@ -146,6 +146,7 @@ type responseduaall struct {
 
 func InitToken(c *fiber.Ctx) error {
 	client := new(clientInit)
+	origin := c.Get("origin")
 	render_page := time.Now()
 	if err := c.BodyParser(client); err != nil {
 		c.Status(fiber.StatusBadRequest)
@@ -161,7 +162,8 @@ func InitToken(c *fiber.Ctx) error {
 		SetResult(responseinitresult{}).
 		SetHeader("Content-Type", "application/json").
 		SetBody(map[string]interface{}{
-			"token": client.Token,
+			"token":    client.Token,
+			"hostname": origin,
 		}).
 		Post(PATH + "api/servicetoken")
 	if err != nil {
@@ -182,6 +184,7 @@ func InitToken(c *fiber.Ctx) error {
 }
 func Listpasaran(c *fiber.Ctx) error {
 	client := new(clientlistpasaran)
+	origin := c.Get("origin")
 	render_page := time.Now()
 	if err := c.BodyParser(client); err != nil {
 		c.Status(fiber.StatusBadRequest)
@@ -198,6 +201,7 @@ func Listpasaran(c *fiber.Ctx) error {
 		SetHeader("Content-Type", "application/json").
 		SetBody(map[string]interface{}{
 			"client_company": client.Company,
+			"hostname":       origin,
 		}).
 		Post(PATH + "api/serviceinit")
 	if err != nil {
@@ -212,6 +216,7 @@ func Listpasaran(c *fiber.Ctx) error {
 }
 func Checkpasaran(c *fiber.Ctx) error {
 	client := new(clientcheckpasaran)
+	origin := c.Get("origin")
 	render_page := time.Now()
 	if err := c.BodyParser(client); err != nil {
 		c.Status(fiber.StatusBadRequest)
@@ -229,6 +234,7 @@ func Checkpasaran(c *fiber.Ctx) error {
 		SetBody(map[string]interface{}{
 			"client_company": client.Company,
 			"pasaran_code":   client.Pasaran_code,
+			"hostname":       origin,
 		}).
 		Post(PATH + "api/servicecheckpasaran")
 	if err != nil {
@@ -256,6 +262,7 @@ func Checkpasaran(c *fiber.Ctx) error {
 }
 func Inittogel_432d(c *fiber.Ctx) error {
 	client := new(clientinitpasaran)
+	origin := c.Get("origin")
 	render_page := time.Now()
 	if err := c.BodyParser(client); err != nil {
 		c.Status(fiber.StatusBadRequest)
@@ -274,6 +281,7 @@ func Inittogel_432d(c *fiber.Ctx) error {
 			"client_company": client.Company,
 			"pasaran_code":   client.Pasaran_code,
 			"permainan":      client.Permainan,
+			"hostname":       origin,
 		}).
 		Post(PATH + "api/serviceconfigtogel")
 	if err != nil {
@@ -297,6 +305,7 @@ func Inittogel_432d(c *fiber.Ctx) error {
 }
 func Clientlimitpasaran(c *fiber.Ctx) error {
 	client := new(clientlimitpasaran)
+	origin := c.Get("origin")
 	render_page := time.Now()
 	if err := c.BodyParser(client); err != nil {
 		c.Status(fiber.StatusBadRequest)
@@ -317,6 +326,7 @@ func Clientlimitpasaran(c *fiber.Ctx) error {
 			"pasaran_code":    client.Pasaran_code,
 			"pasaran_periode": client.Pasaran_periode,
 			"permainan":       client.Permainan,
+			"hostname":        origin,
 		}).
 		Post(PATH + "api/servicelimittogel")
 	if err != nil {
@@ -340,6 +350,7 @@ func Clientlimitpasaran(c *fiber.Ctx) error {
 }
 func Resulttogel(c *fiber.Ctx) error {
 	client := new(clientresult)
+	origin := c.Get("origin")
 	render_page := time.Now()
 	if err := c.BodyParser(client); err != nil {
 		c.Status(fiber.StatusBadRequest)
@@ -357,6 +368,7 @@ func Resulttogel(c *fiber.Ctx) error {
 		SetBody(map[string]interface{}{
 			"client_company": client.Company,
 			"pasaran_code":   client.Pasaran_code,
+			"hostname":       origin,
 		}).
 		Post(PATH + "api/serviceresult")
 	if err != nil {
@@ -381,6 +393,7 @@ func Resulttogel(c *fiber.Ctx) error {
 }
 func ResulttogelAll(c *fiber.Ctx) error {
 	client := new(clientresultall)
+	origin := c.Get("origin")
 	render_page := time.Now()
 	if err := c.BodyParser(client); err != nil {
 		c.Status(fiber.StatusBadRequest)
@@ -397,6 +410,7 @@ func ResulttogelAll(c *fiber.Ctx) error {
 		SetHeader("Content-Type", "application/json").
 		SetBody(map[string]interface{}{
 			"client_company": client.Company,
+			"hostname":       origin,
 		}).
 		Post(PATH + "api/serviceresultall")
 	if err != nil {
@@ -421,6 +435,7 @@ func ResulttogelAll(c *fiber.Ctx) error {
 }
 func Invoicebet(c *fiber.Ctx) error {
 	client := new(clientinvoice)
+	origin := c.Get("origin")
 	render_page := time.Now()
 	if err := c.BodyParser(client); err != nil {
 		c.Status(fiber.StatusBadRequest)
@@ -441,6 +456,7 @@ func Invoicebet(c *fiber.Ctx) error {
 			"client_idinvoice": client.Invoice,
 			"pasaran_code":     client.Pasaran_code,
 			"pasaran_periode":  client.Pasaran_periode,
+			"hostname":         origin,
 		}).
 		Post(PATH + "api/serviceinvoicebet")
 	if err != nil {
@@ -468,6 +484,7 @@ func Invoicebet(c *fiber.Ctx) error {
 }
 func Invoicebetid(c *fiber.Ctx) error {
 	client := new(clientinvoicedetailid)
+	origin := c.Get("origin")
 	render_page := time.Now()
 	if err := c.BodyParser(client); err != nil {
 		c.Status(fiber.StatusBadRequest)
@@ -487,6 +504,7 @@ func Invoicebetid(c *fiber.Ctx) error {
 			"client_company":   client.Company,
 			"client_username":  client.Username,
 			"permainan":        client.Permainan,
+			"hostname":         origin,
 		}).
 		Post(PATH + "api/serviceinvoicebetdetail")
 	if err != nil {
@@ -510,6 +528,7 @@ func Invoicebetid(c *fiber.Ctx) error {
 }
 func Slipperiode(c *fiber.Ctx) error {
 	client := new(clientslip)
+	origin := c.Get("origin")
 	render_page := time.Now()
 	if err := c.BodyParser(client); err != nil {
 		c.Status(fiber.StatusBadRequest)
@@ -528,6 +547,7 @@ func Slipperiode(c *fiber.Ctx) error {
 			"client_company":  client.Company,
 			"client_username": client.Username,
 			"pasaran_code":    client.Pasaran_code,
+			"hostname":        origin,
 		}).
 		Post(PATH + "api/serviceslip")
 	if err != nil {
@@ -551,6 +571,7 @@ func Slipperiode(c *fiber.Ctx) error {
 }
 func SlipperiodeAll(c *fiber.Ctx) error {
 	client := new(clientslipall)
+	origin := c.Get("origin")
 	render_page := time.Now()
 	if err := c.BodyParser(client); err != nil {
 		c.Status(fiber.StatusBadRequest)
@@ -568,6 +589,7 @@ func SlipperiodeAll(c *fiber.Ctx) error {
 		SetBody(map[string]interface{}{
 			"client_company":  client.Company,
 			"client_username": client.Username,
+			"hostname":        origin,
 		}).
 		Post(PATH + "api/serviceslipall")
 	if err != nil {
@@ -591,6 +613,7 @@ func SlipperiodeAll(c *fiber.Ctx) error {
 }
 func Slipperiodedetail(c *fiber.Ctx) error {
 	client := new(clientslipdetail)
+	origin := c.Get("origin")
 	render_page := time.Now()
 	if err := c.BodyParser(client); err != nil {
 		c.Status(fiber.StatusBadRequest)
@@ -609,6 +632,7 @@ func Slipperiodedetail(c *fiber.Ctx) error {
 			"client_company":  client.Company,
 			"client_username": client.Username,
 			"idtrxkeluaran":   client.Idinvoice,
+			"hostname":        origin,
 		}).
 		Post(PATH + "api/serviceslipdetail")
 	if err != nil {
@@ -633,6 +657,7 @@ func Slipperiodedetail(c *fiber.Ctx) error {
 }
 func Savetransaksi(c *fiber.Ctx) error {
 	client := new(clientsavetransaksi)
+	origin := c.Get("origin")
 	render_page := time.Now()
 	if err := c.BodyParser(client); err != nil {
 		c.Status(fiber.StatusBadRequest)
@@ -658,6 +683,7 @@ func Savetransaksi(c *fiber.Ctx) error {
 			"client_username": client.Username,
 			"totalbayarbet":   client.Total,
 			"list4d":          string(client.Data),
+			"hostname":        origin,
 		}).
 		Post(PATH + "api/savetransaksi")
 	if err != nil {
