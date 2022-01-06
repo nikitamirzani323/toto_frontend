438a1791d70dff40813d65d8391f7eaf9be589a7
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2022-01-07 02:03:31 +0700

    Update Path API

--- config/constan.go
+++ config/constan.go
@@ -2,4 +2,4 @@ package config
 
 // const PATH_API string = "http://10.104.0.14:7071/"
 
-const PATH_API string = "http://139.59.105.180:7071/"
+const PATH_API string = "http://128.199.241.112:7071/"
